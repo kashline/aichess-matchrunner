317eaa73ee0e0486bc47d9eee2132a6e7f6547bf
Kevin Ashline <kevin.ashline92@gmail.com>
2025-06-13 09:42:08 -0700

    in gcp

--- deploy.sh
+++ deploy.sh
@@ -10,13 +10,24 @@ else
 fi
 
 # Define the image name
-IMAGE_NAME="aichess-matchrunner"
-PROJECT="aichess-457721"
+# IMAGE_NAME="aichess-matchrunner"
+# PROJECT="aichess-457721"
+
+AWS_REGION="us-west-2"
+ACCOUNT_ID="079152373621"
+REPO_NAME="aichess-matchrunner"
+IMAGE_TAG="latest"
+
+IMAGE_URI="${ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com/${REPO_NAME}:${IMAGE_TAG}"
+
+aws ecr get-login-password --region $AWS_REGION | \
+  docker login --username AWS --password-stdin "${ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com"
+
 
 # Build the Docker image
 echo "Building Docker image..."
-docker buildx build --platform linux/amd64 -t $IMAGE_NAME .
+docker buildx build --platform linux/amd64 -t $IMAGE_URI .
 
-docker tag aichess-matchrunner:latest us-west1-docker.pkg.dev/$PROJECT/$IMAGE_NAME/$IMAGE_NAME:latest
+# docker tag aichess-matchrunner:latest us-west1-docker.pkg.dev/$PROJECT/$IMAGE_NAME/$IMAGE_NAME:latest
 
-docker push us-west1-docker.pkg.dev/$PROJECT/$IMAGE_NAME/$IMAGE_NAME:latest
+docker push "$IMAGE_URI"
